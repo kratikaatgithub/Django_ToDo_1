80605cf408422057f9bdb9d62f468d55ba9258d1
Kratika <kratikag1@gmail.com>
2021-09-24 23:01:16 +0530

    Commit - 4

--- tasks/views.py
+++ tasks/views.py
@@ -4,29 +4,6 @@ from django.http import HttpResponse
 from .models import *
 from .forms import *
 
-
-#from django.db import models
-
-# Create your models here.
-#class Task(models.Model):
- #   title = models.CharField(max_length=200)
-  #  complete = models.BooleanField(default=False)
-   # created = models.DateTimeField(auto_now_add=True)
-
-    #def __str__(self):
-     #   return self.title
-
-#from django import forms
-#from django.forms import ModelForm
-
-#from models import *
-
-#class TaskForm(forms.ModelForm):
- #   class Meta:
-    #    model = Task
-     #   fields = '__all__'
-
-
 def index(request):
     tasks = Task.objects.all()
 
@@ -43,4 +20,20 @@ def index(request):
 
 def updateTask(request, pk):
      task = Task.objects.get(id=pk)
-     return render(request, 'tasks/update_task.html')
+     form = TaskForm(instance=task)
+     if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+              form.save()
+              return redirect('/')
+
+     context = {'form':form}
+     return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+     item = Task.objects.get(id=pk)
+     if request.method == 'POST':
+          item.delete()
+          return redirect('/')
+     context = {'item':item}
+     return render(request, 'tasks/delete.html')
